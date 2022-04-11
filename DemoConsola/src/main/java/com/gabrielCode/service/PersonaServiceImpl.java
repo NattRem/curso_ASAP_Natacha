675d9c60c8c3740aef70a91826bcd6d5e9621434
NattRem <remnatt4@gmail.com>
2022-04-11 18:49:26 -0300

    Comence con la practica 6 Spring Boot Qualifier

---
+++ DemoConsola/src/main/java/com/gabrielCode/service/PersonaServiceImpl.java
@@ -0,0 +1,18 @@
+package com.gabrielCode.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.gabrielCode.repository.IPersona;
+
+public class PersonaServiceImpl implements IpersonaService{
+	
+	@Autowired
+	@Qualifier("persona2")
+	IPersona repo;
+	@Override
+	public void registrarHandler(String pNombre) {
+		repo.registrar(pNombre);		
+	}
+	
+}
